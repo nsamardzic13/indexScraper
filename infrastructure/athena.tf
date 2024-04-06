7f15fae74505a562f79e7bf052189efb1d584113
nsamardzic13 <nikola.samardzic1997@gmail.com>
2024-04-06 16:43:02 +0200

    added athena workgroup

---
+++ infrastructure/athena.tf
@@ -0,0 +1,16 @@
+resource "aws_s3_bucket" "tf_indexads_athena_bucket" {
+  bucket = "${var.project_name}-athena-bucket"
+}
+
+resource "aws_athena_workgroup" "example" {
+  name = "${var.project_name}-athena-workgroup"
+
+  configuration {
+    enforce_workgroup_configuration    = true
+    publish_cloudwatch_metrics_enabled = true
+
+    result_configuration {
+      output_location = "s3://${aws_s3_bucket.tf_indexads_athena_bucket.bucket}/output/"
+    }
+  }
+}
--- infrastructure/s3.tf
+++ infrastructure/s3.tf
@@ -2,7 +2,3 @@ resource "aws_s3_bucket" "tf_indexads_bucket" {
   bucket = "${var.project_name}-bucket"
 
 }
-
-resource "aws_s3_bucket" "tf_indexads_athena_bucket" {
-  bucket = "${var.project_name}-athena-bucket"
-}
