0862354209cfb5a662695fd510215f1c17e6280d
nsamardzic13 <nikola.samardzic1997@gmail.com>
2024-05-10 00:07:01 +0200

    stopping execution

--- infrastructure/eventbridge.tf
+++ infrastructure/eventbridge.tf
@@ -1,15 +1,15 @@
-resource "aws_scheduler_schedule" "example" {
-  name       = "${var.project_name}-schedule"
-  group_name = "default"
+# resource "aws_scheduler_schedule" "example" {
+#   name       = "${var.project_name}-schedule"
+#   group_name = "default"
 
-  flexible_time_window {
-    mode = "OFF"
-  }
+#   flexible_time_window {
+#     mode = "OFF"
+#   }
 
-  schedule_expression = "cron(00 07 ? * MON,THU *)"
+#   schedule_expression = "cron(00 07 ? * MON,THU *)"
 
-  target {
-    arn      = aws_sfn_state_machine.tf_indexads_sfn.arn
-    role_arn = aws_iam_role.tf_indexads_role.arn
-  }
-}
+#   target {
+#     arn      = aws_sfn_state_machine.tf_indexads_sfn.arn
+#     role_arn = aws_iam_role.tf_indexads_role.arn
+#   }
+# }
